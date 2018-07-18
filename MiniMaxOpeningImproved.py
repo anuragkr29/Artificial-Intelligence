347622074d889da10235c0bd2c24ae5f7457dd65
anuragkumar <anuragkr29@gmail.com>
2018-07-18 02:47:31 -0500

    Improved static estimate function

---
+++ MiniMaxOpeningImproved.py
@@ -0,0 +1,17 @@
+from functions import morrisGame
+import IO
+
+
+def main():
+    root, outputFile, depth = IO.getInput()
+    try:
+        game = morrisGame()
+        estimate, next_move= game.run('opening', 'improved', root, depth, True)
+        IO.writeToFile(outputFile, next_move)
+        game.printOutput(estimate, 'MiniMax')
+    except Exception as e:
+        print("An Error occured :",e)
+
+
+if __name__ == "__main__":
+    main()
